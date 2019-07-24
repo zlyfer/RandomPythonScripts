203c965ddbf27e1772b0a8886fd4d986e186ca4b
zlyfer <zlyfer@protonmail.com>
2019-07-24 10:50:50 +0200

    update i guess

--- DBTest.py
+++ DBTest.py
@@ -3,7 +3,7 @@ from time import sleep
 while True:
     sleep(5)
     try:
-        db=MySQLdb.connect(host="127.0.0.1", user="root", passwd="devemode", db="xmlp")
+        db=MySQLdb.connect(host="127.0.0.1", user="root", passwd="", db="xmlp")
     except MySQLdb.Error:
         #os.system("systemctl restart mysql")
         print("err")
